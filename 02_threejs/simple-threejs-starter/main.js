b9d218d254eda2056ddda794fffce8fec32a0b80
VanstiW <mw3627@columbia.edu>
2023-07-20 06:43:26 +0800

    js1

---
+++ 02_threejs/simple-threejs-starter/main.js
@@ -0,0 +1,11 @@
+console.log('this works')
+// import the THREE library
+import * as THREE from 'three';
+
+// get a reference to the scene-container element that will eventually hold the scene
+
+// create a scene
+
+// create a camera
+
+// create the renderer
